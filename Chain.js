a52d8d125c144151df54111143262032c1b7196c
kashishGoyal2009 <71822158+kashishGoyal2009@users.noreply.github.com>
2020-12-02 17:17:16 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,19 @@
+class Chain  {
+    constructor(body1,body2){
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:.04,
+            length:10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain)  
+    }
+  
+    display() {
+    var pos1=this.chain.bodyA.position
+    var pos2=this.chain.bodyB.position
+    line(pos1.x,pos1.y,pos2.x,pos2.y)
+    }
+  }
+  
